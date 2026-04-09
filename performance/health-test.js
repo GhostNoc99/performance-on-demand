5370147596cef71898c4a314fce0144bfcde0ba2
GhostNoc99 <newprojectcv.1999@gmail.com>
2026-04-09 00:07:04 -0500

    cambio URL por la de Docker

--- performance/health-test.js
+++ performance/health-test.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { htmlReport } from 'https://jslib.k6.io/k6-summary/0.1.0/index.js';
 
-const BASE_URL = __ENV.BASE_URL || 'http://host.docker.internal:8000/health';
+const BASE_URL = __ENV.BASE_URL || 'http://host.docker.internal:8000';
 
 export const options = {
   vus: 1,
